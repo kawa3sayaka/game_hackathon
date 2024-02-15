13b2f87ea4c32ac45105dc65eb98693f106042ea
kawa3sayaka <115607345+kawa3sayaka@users.noreply.github.com>
2024-02-15 15:11:10 +0900

    カードの正誤判定

--- memory-app/src/App.js
+++ memory-app/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import React, {useState} from "react";
+import React, { useEffect, useState} from "react";
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  {"src": "/img/eating.jpg"},
-  {"src": "/img/kitsune.jpg"},
-  {"src": "/img/nekogyo.jpg"},
-  {"src": "/img/rakuda.jpg"},
-  {"src": "/img/saboten.jpg"},
-  {"src": "/img/snake.jpg"}
+  {"src": "/img/one.jpg", matched: false },
+  {"src": "/img/two.jpg", matched: false },
+  {"src": "/img/three.jpg", matched: false },
+  {"src": "/img/four.jpg", matched: false },
+  {"src": "/img/five.jpg", matched: false },
+  {"src": "/img/six.jpg", matched: false }
 ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  //自動的に新しいゲームを始める
+  useEffect(()=> {
+    shuffleCards()
+  }, [])
 
   // カードをシャッフル
   const shuffleCards = () =>{
@@ -20,27 +29,67 @@ function App() {
       .sort(()=> Math.random() - 0.5)
       .map((card)=>({ ...card, id: Math.random() }))
 
+      setChoiceOne(null)
+      setChoiceTwo(null)
       setCards(shuffledCards)
       setTurns(0)
   }
 
-  console.log(cards, turns)
+  //一枚目に選んだカードかどうか判定して、変数に代入
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  //判定
+  useEffect(()=>{
+    
+    if(choiceOne && choiceTwo){
+      setDisabled(true)
+
+      if(choiceOne.src === choiceTwo.src){
+        setCards(prevCards => {
+          return prevCards.map(card =>{
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            }else{
+              return card
+            }
+          })
+        })
+        resetTurn()
+      }else{
+        //1000ミリ秒後に裏返しに戻す
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  },[choiceOne, choiceTwo])
+
+  //選んだカードのリセット＆回数制限を追加
+  const resetTurn = () =>{
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
 
   return (
     <div className="App">
-      <h1>おもちモチモチ</h1>
+      <h1>絵あわせ</h1>
+      
       <button onClick = {shuffleCards}>START</button>
 
       <div className = "card-grid">
         {cards.map(card => (
-          <div className = "card" key = {card.id}>
-              <div>
-                <img className = "front" src = {card.src} alt="card front" />
-                <img className = "back" src="/img/background.png" alt="card back" />
-              </div>
-            </div>
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice = {handleChoice}
+            flipped = {card === choiceOne || card === choiceTwo || card.matched}
+            disabled = {disabled}
+            />
         ))}
       </div>
+      <h2>ターン： {turns}</h2>
     </div>
   );
 }
